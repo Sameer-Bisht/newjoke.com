e4f532fc2f52e529004b740f77f591641473dc6f
Sameer-Bisht <98872629+Sameer-Bisht@users.noreply.github.com>
2023-01-07 16:32:51 +0530

    new working update by me

--- app.js
+++ app.js
@@ -7,15 +7,11 @@ const port = process.env.PORT || 3000;
 app.use(express.static('static'))
 app.use(urlencoded());
 
-app.set('viewengine','html');
-app.set('views',path.join(__dirname,'views'))
-
 // ENDPOINTS 
 app.get("/", (req,res)=>{
-    res.status(200).render('40.html')
+    res.sendFile(path.join(__dirname+'/views/40.html'));
 })
     
-
 // Listening to server
 app.listen(port,()=>{
     console.log("THisis working ");
